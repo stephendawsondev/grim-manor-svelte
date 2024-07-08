09392d6a72eba3de038ec2310291a3befcfc3bc6
Stephen Dawson <stephendawson@protonmail.com>
2024-07-08 14:25:36 +0100

    chore: rename script and remove old code

--- src/lib/script.svelte.ts
+++ src/lib/script.svelte.ts
@@ -1,9 +1,9 @@
 // jshint esversion: 6
 
 // Minigames code
-import { startMemoryGame } from './minigames/memory-game.js';
-import { handlePlay } from './minigames/quiz.js';
-import { initLastQuizGame } from './minigames/lastquiz.js';
+import { startMemoryGame } from '/js/minigames/memory-game.js';
+import { handlePlay } from '/js/minigames/quiz.js';
+import { initLastQuizGame } from '/js/minigames/lastquiz.js';
 import { showDialogueAsync } from '$lib/utils/dialogueGeneration.js';
 import { savePlayerData, updateLocalProperty } from '$lib/utils/gamedataLocalstore.js';
 import {
@@ -26,61 +26,14 @@ const playMusicOnLoop = (audio) => {
 
 document.addEventListener('DOMContentLoaded', async () => {
 	// Mansion interaction code
-	const backgroundImage = document.getElementById('background-image'); // Select the background image
-	const moveButtons = document.querySelectorAll('button'); // Select all buttons with the "move" class
-	const mansionView = document.getElementById('mansion-container'); // Select the container with the mansion image
 	const gameContainer = document.getElementById('game-container'); // Select the container for the minigames
-	const interactiveButtons = document.querySelectorAll('.interactive'); // Select all buttons (doors) with the "interactive" class
-
-	// let loadedPlayerData = loadPlayerData();
-	// const audioObj = loadAudio(url_audios_deploy);
-	// addAudioIconEventListeners(url_audios_deploy);
 
 	const minigames = document.querySelectorAll('.minigame');
 	for (const minigame of minigames) {
 		minigame.classList.remove('active');
 	}
 
-	// if esc key is pressed, loop through
-	// minigames and remove active class
-	window.addEventListener('keydown', (e) => {
-		if (e.key === 'Escape') {
-			for (const minigame of minigames) {
-				minigame.classList.remove('active');
-			}
-			gameContainer.className = '';
-			gameContainer.close();
-		}
-		if (playerAllowsMusic) {
-			audioObj.creepyWhistlyMusic.pause();
-			playMusicOnLoop(audioObj.darkAmbientMusic);
-		}
-	});
-
-	// loop through 'interactive' and disable
-	for (const button of interactiveButtons) {
-		button.disabled = true;
-		button.classList.add('disabled');
-		button.setAttribute('aria-disabled', 'true');
-		// Add a click event to each button
-		button.addEventListener('click', function () {
-			displayMiniGames(this.id);
-		});
-	}
-	// if (loadedPlayerData.firstTimePlaying) {
-	// 	await showDialogueAsync(introDialogue);
-	// 	updateLocalProperty('firstTimePlaying', false);
-	// } else {
 	await showDialogueAsync(returnedButIncomplete);
-	// }
-
-	// loop through 'interactive' and enable
-	for (const button of interactiveButtons) {
-		button.disabled = false;
-		button.classList.remove('disabled');
-		button.setAttribute('aria-disabled', 'false');
-	}
-	// ghost.classList.remove('active');
 
 	// -------- Mini games functions ---------
 
@@ -138,20 +91,5 @@ document.addEventListener('DOMContentLoaded', async () => {
 		}
 		gameContainer.showModal();
 		handlePlay();
-	};
-
-	/*This function finds the id of the clicked button
-	 * passed from the event listener
-	 * and calls the respective function
-	 * to displays the clicked mini game
-	 */
-	const displayMiniGames = (id) => {
-		if (id == 'door2') {
-			// miniGame2();
-		} else if (id == 'door3') {
-			miniGame3();
-		} else if (id == 'door4') {
-			miniGame4();
-		}
 	};
 });
