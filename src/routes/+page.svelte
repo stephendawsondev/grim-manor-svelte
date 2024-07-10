1e4f591a0e59e62a2e2eb3a3bbf66b8bc48fee96
Stephen Dawson <stephendawson@protonmail.com>
2024-07-10 16:36:25 +0100

    feat: update landing to display mansion first visit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
-<script>
+<script lang="ts">
 	import MansionSplash from '$lib/components/MansionSplash.svelte';
 	import Mansion from '$lib/components/Mansion.svelte';
+	import { getUserDataContext } from '$lib/index.svelte';
+
+	let userData = getUserDataContext().value;
 </script>
 
-<MansionSplash />
-<Mansion />
+{#if userData}
+	{#if userData.landingPageComplete == true}
+		<Mansion />
+	{:else}
+		<MansionSplash />
+	{/if}
+{:else}
+	<p>Loading user data...</p>
+{/if}
