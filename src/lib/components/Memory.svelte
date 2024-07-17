9c20abba2d8765e16c7fcdf228208d213f717cc7
Stephen Dawson <stephendawson@protonmail.com>
2024-07-17 21:35:47 +0100

    feat: add dialogues to memory game

--- src/lib/components/Memory.svelte
+++ src/lib/components/Memory.svelte
@@ -3,23 +3,51 @@
 	import MemoryCard from './MemoryCard.svelte';
 	import { getUserDataContext, getAudioManagerContext } from '$lib/index.svelte';
 	import { showDialogueAsync } from '$lib/utils/dialogueGeneration';
-	import { memoryGameInitialDialogue, memoryGameEndDialogue } from '$lib/utils/dialogues';
+	import type { DialogueItem, UserData } from '$lib/types';
 
 	let { updateContainerBackground }: { updateContainerBackground: (background: string) => void } =
 		$props();
 
-	interface UserData {
-		playerName: string;
-		firstTimePlaying: boolean;
-		landingPageComplete: boolean;
-		storyComplete: boolean;
-		hangmanClueObtained: boolean;
-		memoryClueObtained: boolean;
-		quizClueObtained: boolean;
-		backDoorOpened: boolean;
-		playerAllowsMusic: boolean;
-		playerAllowsSound: boolean;
-	}
+	let showEndCard: boolean = $state(false);
+
+	const memoryGameInitialDialogue: DialogueItem[] = [
+		{
+			text: 'You walk to the stairs, where a young boy is sitting and playing a card game. His clothing is old, from another time. '
+		},
+		{
+			text: 'He is not transparent as the first young man you encountered was, but there is a translucence to his skin.'
+		},
+		{
+			text: 'He looks at you with haunted eyes.'
+		},
+		{
+			text: '“To discover the secrets of this house, choose the correct cards but you must remember where they lie...”'
+		}
+	];
+
+	const memoryGameEndDialogue: DialogueItem[] = [
+		{
+			text: 'In his high child’s voice, the boy says:'
+		},
+		{
+			text: '“The man who owned this house hung himself many years ago…”'
+		},
+		{
+			text: '“They say his spirit still haunts these halls...”'
+		}
+	];
+
+	const returnIntroDialogue: DialogueItem[] = [
+		{
+			text: '"Have you come to play with me again?"'
+		}
+	];
+
+	const returnWinDialogue: DialogueItem[] = [
+		{
+			text: '"Please don\'t leave me alone..."'
+		}
+	];
 
 	let userData = getUserDataContext().value as UserData;
 	let audioManager = getAudioManagerContext();
@@ -64,9 +92,15 @@
 
 			matchedPairs++;
 			if (matchedPairs === totalPairs) {
+				if (userData.memoryClueObtained) {
+					await showDialogueAsync(returnWinDialogue);
+				} else {
+					await showDialogueAsync(memoryGameEndDialogue);
+				}
 				userData.memoryClueObtained = true;
 				document.getElementById('memory-game')!.classList.remove('active');
-				await showDialogueAsync(memoryGameEndDialogue, true);
+
+				showEndCard = true;
 			}
 
 			resetBoard();
@@ -106,7 +140,11 @@
 			audioManager.playAudioLoop('creepyWhistlyMusic');
 		}
 
-		await showDialogueAsync(memoryGameInitialDialogue, true);
+		if (userData.memoryClueObtained) {
+			await showDialogueAsync(returnIntroDialogue);
+		} else {
+			await showDialogueAsync(memoryGameInitialDialogue);
+		}
 		startMemoryGame();
 	});
 
@@ -126,9 +164,47 @@
 		<MemoryCard {cardName} />
 	{/each}
 </div>
+{#if showEndCard}
+	<div class="card end-card">
+		<p>Leave now?</p>
+		<a href="/" class="btn exit-btn">Exit</a>
+	</div>
+{/if}
 
 <style>
 	/* Memory mini-game styles */
+	.card {
+		background-color: #2a2a2a;
+		padding: 20px;
+		border-radius: 8px;
+		margin: 10px 0;
+		width: 100%;
+		max-width: 500px;
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%);
+		color: #fafafa;
+
+		.btn {
+			background-color: #fafafa;
+			color: #1a1a1a;
+			border: none;
+			padding: 10px 20px;
+			margin: 5px;
+			border-radius: 4px;
+			cursor: pointer;
+			font-weight: bold;
+			text-decoration: none;
+			transition: all 0.3s ease;
+			&:hover {
+				background-color: #1a2227;
+				color: #fafafa;
+				transform: scale(1.05);
+			}
+		}
+	}
+
 	#memory-game {
 		width: 100%;
 		height: 100%;
